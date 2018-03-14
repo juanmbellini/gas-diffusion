496bdb849f6146a24f12846222619fc280d25111
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-03-14 10:40:19 -0300

    Move SimulationArguments class into io package

--- src/main/java/ar/edu/itba/ss/off_lattice/SelfPropelledFlockSimulator.java
+++ src/main/java/ar/edu/itba/ss/off_lattice/SelfPropelledFlockSimulator.java
@@ -1,14 +1,13 @@
 package ar.edu.itba.ss.off_lattice;
 
+import ar.edu.itba.ss.off_lattice.io.SimulationArguments;
 import ar.edu.itba.ss.off_lattice.simulation.SimulationEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 /**
  * Main class.
@@ -68,66 +67,6 @@ public class SelfPropelledFlockSimulator implements CommandLineRunner {
         // TODO: implement raw file (to be used in case anything else must be calculated)
         // TODO: implement Ovito file
         // TODO: implement matlab/octave files
-    }
-
-    /**
-     * Bean class used for getting values from properties by injection.
-     *
-     * @apiNote This is a package visible class in order to be scanned by Spring.
-     */
-    @Component
-    /* package */ static final class SimulationArguments {
-
-        /**
-         * The amount of iterations.
-         */
-        private final int iterations;
-
-        /**
-         * The 'eta' value, used for calculating noise for updating angles.
-         */
-        private final double eta;
-
-        /**
-         * The 'M' value used by cell index method.
-         */
-        private final int m;
-
-
-        /**
-         * @param iterations The amount of iterations.
-         * @param eta        The 'eta' value, used for calculating noise for updating angles.
-         * @param m          The 'm' value used by cell index method.
-         */
-        @Autowired
-        private SimulationArguments(@Value("${custom.simulation.iterations}") int iterations,
-                                    @Value("${custom.simulation.eta}") double eta,
-                                    @Value("${custom.simulation.M}") int m) {
-            this.iterations = iterations;
-            this.eta = eta;
-            this.m = m;
-        }
-
-        /**
-         * @return The amount of iterations.
-         */
-        private int getIterations() {
-            return iterations;
-        }
-
-        /**
-         * @return The 'eta' value, used for calculating noise for updating angles.
-         */
-        private double getEta() {
-            return eta;
-        }
-
-        /**
-         * @return The 'M' value used by cell index method.
-         */
-        private int getM() {
-            return m;
-        }
     }
 
     /**
